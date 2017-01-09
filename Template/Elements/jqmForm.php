1dd08db36d39cd1ce1b9d0488336eeda5f5b24ba
Andrej Kabachnik <andrej_ka@gmx.de>
2017-01-09 09:23:37 +0100

    FIX removed differences between form and panel widgets

--- Template/Elements/jqmForm.php
+++ Template/Elements/jqmForm.php
@@ -1,26 +1,13 @@
 <?php
 namespace exface\JQueryMobileTemplate\Template\Elements;
+
+/**
+ * 
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
 class jqmForm extends jqmPanel {
 	
-	function generate_html(){
-		$output = '';
-		if ($this->get_widget()->get_caption()){
-			$output = '<div class="ftitle">' . $this->get_widget()->get_caption() . '</div>';
-		}
-		
-		$output .= '<form id="' . $this->get_widget()->get_id() . '">';
-		$output .= $this->build_html_for_widgets();					
-		$output .= '</form>';
-		
-		return $output;
-	}
-	
-	public function get_method() {
-		return $this->method;
-	}
-	
-	public function set_method($value) {
-		$this->method = $value;
-	}
 }
 ?>
