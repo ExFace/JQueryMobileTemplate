6729e8fc211a0de9736a27deafab47ec6178b7d8
aka <andrej.kabachnik@salt-solutions.de>
2017-09-05 10:31:19 +0200

    REFACTOR legacy method names

--- Template/Elements/jqmButton.php
+++ Template/Elements/jqmButton.php
@@ -86,7 +86,7 @@ class jqmButton extends jqmAbstractElement
         $widget = $this->getWidget();
         if ($action->getPageId() != $this->getPageId()) {
             $output = $this->buildJsRequestDataCollector($action, $input_element) . "
-				 	$.mobile.changePage('" . $this->getTemplate()->createLinkInternal($action->getPageId()) . "?prefill={\"meta_object_id\":\"" . $widget->getMetaObjectId() . "\",\"rows\":[{\"" . $widget->getMetaObject()->getUidAlias() . "\":' + requestData.rows[0]." . $widget->getMetaObject()->getUidAlias() . " + '}]}');";
+				 	$.mobile.changePage('" . $this->getTemplate()->createLinkInternal($action->getPageId()) . "?prefill={\"meta_object_id\":\"" . $widget->getMetaObjectId() . "\",\"rows\":[{\"" . $widget->getMetaObject()->getUidAttributeAlias() . "\":' + requestData.rows[0]." . $widget->getMetaObject()->getUidAttributeAlias() . " + '}]}');";
         }
         return $output;
     }
