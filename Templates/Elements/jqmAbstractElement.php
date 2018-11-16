37fcf0eba4f7c7aa5f9f6194908e2fec708a9d17
Andrej Kabachnik <andrej_ka@gmx.de>
2018-11-16 16:20:52 +0100

    REFACTOR for compatibility with separated count() operation

--- Templates/Elements/jqmAbstractElement.php
+++ Templates/Elements/jqmAbstractElement.php
@@ -50,8 +50,8 @@ abstract class jqmAbstractElement extends AbstractJqueryElement
         
         $data = array();
         $data['data'] = $data_sheet->getRows();
-        $data['recordsFiltered'] = $data_sheet->countRowsAll();
-        $data['recordsTotal'] = $data_sheet->countRowsAll();
+        $data['recordsFiltered'] = $data_sheet->countRowsInDataSource();
+        $data['recordsTotal'] = $data_sheet->countRowsInDataSource();
         $data['footer'] = $data_sheet->getTotalsRows();
         return $data;
     }
